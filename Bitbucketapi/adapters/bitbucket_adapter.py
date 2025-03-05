6dd262c636493cb04b8df58f1dc3f1bb79c37cab
Sushanth K S <sushanth.ks@amadeus.com>
2025-03-05 15:54:45 +0530

    Fixed issues

--- Bitbucketapi/adapters/bitbucket_adapter.py
+++ Bitbucketapi/adapters/bitbucket_adapter.py
@@ -5,22 +5,24 @@ from git import Repo
 
 
 class BitbucketAdapter(BaseAdapter):
-    def __init__(self,repo_url,):
-        self.base_url="https://rndwww.nce.amadeus.net/git/scm/ahfr/obe-hcr-ifd.git" #FILL URL
-        self.auth=(username,app_password)
+    def __init__(self,repo_url,local_path=r"C:\Users\sks\OneDrive - Amadeus Workplace\Documents\P_queue\obe-hcr-ifd"):
+        self.repo_url="https://rndwww.nce.amadeus.net/git/scm/ahfr/obe-hcr-ifd.git" #FILL URL
+        self.local_path=local_path
 
-    def get_commits(self, repo_name, branch):
-        url = f"https://rndwww.nce.amadeus.net/git/scm/ahfr/obe-hcr-ifd.git"  #FILL URL
-        response=requests.get(url,auth=self.auth)
-        
-        if response.status_code=='200':
-            commits=response.json()["values"]
-            return [
-                {"Hash":commit["hash"],
-                 "message":commit["message"],
-                 "author":commit["author"]}
-                 for commit in commits
-            ]
-        else: 
-            print(f"Failed to retrieve the file: details: {response.json()}")
+    def get_commits(self, branch="release/5"):
+        #url = f"https://rndwww.nce.amadeus.net/git/scm/ahfr/obe-hcr-ifd.git"  #FILL URL
+        #response=requests.get(url,auth=self.auth)
+        repo=Repo(self.local_path)
+        commits=[]
+        #if response.status_code=='200':
+        for commit in repo.iter_commits(branch):
+            #commits=response.json()["values"]
+            commits.append(
+                {"Hash":commit.hexsha,
+                 "message":commit.message.strip(),
+                 "author":commit.author.name}
+            )
+        return commits
+        #else:
+            #print(f"Failed to retrieve the file")
         
